f8a0dc2635a289221a0ff1df8629ac61cae1c77d
vkazakevich <vkazakevich@users.noreply.github.com>
2022-01-14 18:03:02 +0000

    deploy: 4f03eb14ab2a2b82c5f5ce66536f40785dae0986

---
+++ component---src-pages-404-js-2e4f38e4378d75cc12f6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvkazakevich=self.webpackChunkvkazakevich||[]).push([[883],{9616:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var u=t(7294),a=t(5444);function c(){return(0,u.useEffect)((function(){(0,a.navigate)("/")}),[]),u.createElement("div",null,u.createElement("h1",null,"(404) NotFound Page"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2e4f38e4378d75cc12f6.js.map
