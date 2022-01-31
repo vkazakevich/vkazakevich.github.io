beed8fad976e61570105ee87213a0143a0311a64
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2022-01-31 17:33:38 +0100

    Feat: Update photo

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 export default function Layout ({ children }) {
   return (
     <div className="flex flex-col h-screen bg-gray-900 text-gray-400">
-      <div className="h-2.5 bg-indigo-600"/>
+      <div className="h-2.5 bg-blue-500"/>
       <div className="flex flex-grow justify-center items-center">
         <div className="container px-8">
           {children}
